f3c274c400b9cf6976e1e2b96b2247c3e90cb6a6
Brian Hiatt <brian.hiatt41@gmail.com>
2014-09-04 22:02:17 -0500

    added test files

---
+++ test/unit/gift.js
@@ -0,0 +1,40 @@
+/* jshint expr:true */
+/* global describe, it, before, beforeEach */
+
+'use strict';
+
+var expect    = require('chai').expect,
+    Gift      = require('../../app/models/gift'),
+    dbConnect = require('../../app/lib/mongodb'),
+    cp        = require('child_process'),
+    db        = 'white-van-test';
+
+describe('Gift', function(){
+  before(function(done){
+    dbConnect(db, function(){
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      done();
+    });
+  });
+
+  describe('constructor', function(){
+    it('should create a new Gift object', function(){
+      var g = new Gift();
+      expect(g).to.be.instanceof(Gift);
+    });
+  });
+
+  describe('.findById', function(){
+    it('should find a gift by it\'s id', function(done){
+      Gift.findById({}, function(err, gift){
+        done();
+      });
+    });
+  });
+});
+
---
+++ test/unit/message.js
@@ -0,0 +1,38 @@
+/* jshint expr:true */
+/* global describe, it, before, beforeEach */
+
+'use strict';
+
+var expect    = require('chai').expect,
+    Message   = require('../../app/models/message'),
+    dbConnect = require('../../app/lib/mongodb'),
+    cp        = require('child_process'),
+    db        = 'white-van-test';
+
+describe('Message', function(){
+  before(function(done){
+    dbConnect(db, function(){
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      done();
+    });
+  });
+
+  describe('constructor', function(){
+    it('should create a new Message object', function(){
+      var m = new Message();
+      expect(m).to.be.instanceof(Message);
+    });
+  });
+
+  describe('.findById', function(){
+    it('should find a message by it\'s id', function(done){
+      done();
+    });
+  });
+});
+
