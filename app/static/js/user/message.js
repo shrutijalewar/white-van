891943b63002ad86dcfe824c74b96d437582f228
Kayla Jones <KaylaLyne424@gmail.com>
2014-09-06 16:03:02 -0500

    added dro markers to the map, added feature where subject is hidden in messages when text radio btn is focused on

---
+++ app/static/js/user/message.js
@@ -0,0 +1,20 @@
+(function(){
+  'use strict';
+
+  $(document).ready(function(){
+    $('#text').focus(hideSubject);
+    $('input[name=mType]:not(#text)').focus(showSubject);
+  });
+
+  function hideSubject(e){
+    var $input = $(e.target);
+    $input.parents('.form-group').find('#subject').fadeOut()
+  }
+  function showSubject(e){
+    var $input = $(e.target);
+    $input.parents('.form-group').find('#subject').fadeIn()
+  }
+
+})();
+
+
