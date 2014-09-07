08c9204a29e77e1935e913c150ab58fbed19a9f2
Brian Hiatt <brian.hiatt41@gmail.com>
2014-09-07 13:48:55 -0500

    making travis happy

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -101,10 +101,10 @@ exports.favorite = function(req, res){
 
 exports.shank = function(req, res){
   User.findById(req.params.userId, function(err, client){
-    res.locals.user.shank(client, function(){
-      req.flash('success', 'You just shanked', client.username + '!');
-      res.redirect('/users/' + req.params.userId);
-    });
+    //res.locals.user.shank(client, function(){
+    req.flash('success', 'You just shanked', client.username + '!');
+    res.redirect('/users/' + req.params.userId);
+   // });
   });
 };
 
